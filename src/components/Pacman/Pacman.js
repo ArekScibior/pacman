0397851074ac8669672ac13b5486f4143a3f6f26
Ścibior Arek <scibior.arek@gmail.com>
2020-09-26 11:14:53 +0200

    PacmanSvg: add pacmanSvg as component and styled

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import './style.css';
-
+import {ReactComponent as PacmanSvg} from '../../assets/pacman.svg';
 class Pacman extends Component {
+
+    state = {
+        direction: 'right',
+
+        position: {
+            top: 5,
+            left: 5,
+        }
+    }
+
     render() {
         return (
-            <div className='pacman'>
-                Hello pacman
+            <div 
+                className='pacman'
+                style={this.state.position}
+            >
+                <PacmanSvg />
             </div>
         )
     }
+}
+
+Pacman.defultProps = {
+    //all in px;
+    step: 50, 
+    size: 50,
+    border: 20,
+    topScoreboardHeight: 50
 }
 
 export default Pacman;

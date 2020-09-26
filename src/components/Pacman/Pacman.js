e173769b1c53607eb398f9c37fee1ce2f4503d08
Ścibior Arek <scibior.arek@gmail.com>
2020-09-26 11:00:39 +0200

    Pacman: add pacman component

---
+++ src/components/Pacman/Pacman.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './style.css';
+
+class Pacman extends Component {
+    render() {
+        return (
+            <div className='pacman'>
+                Hello pacman
+            </div>
+        )
+    }
+}
+
+export default Pacman;
+
